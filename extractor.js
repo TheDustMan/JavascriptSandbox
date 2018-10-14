0d0a44840c73af10ecbf182dc5ec6fa354bcbf84
Dustin Aguilar <dustin@nugen.io>
2018-10-13 22:35:10 -0700

    Created web of latitude lines

--- extractor.js
+++ extractor.js
@@ -1,5 +1,5 @@
 export class Extractor {
-    constructor(spawner, origin, directions, rate, scale, memory, scene)
+    constructor(spawner, origin, directions, rate, scale, memory, latitudeSpacing, scene)
     {
         this.spawner = spawner;
         this.origin = origin;
@@ -9,7 +9,13 @@ export class Extractor {
         this.memory = memory;
         this.points = [];
         this.colors = [];
-        this.lines = [];
+        this.lineMeshes = [];
+
+        this.latitudeSpacing = latitudeSpacing;
+        this.latitudeLines = []
+        this.latitudeLineColors = []
+        this.latitudePointColors = []
+        this.latitudeLineMeshes = [];
         
         this.build(scene);
     }
@@ -29,13 +35,45 @@ export class Extractor {
             this.points.push(points);
             this.colors.push(colors);
 
-            this.lines.push(BABYLON.MeshBuilder.CreateLines("lines",
+            this.lineMeshes.push(BABYLON.MeshBuilder.CreateLines("lines",
                                                             {
                                                                 points: points,
                                                                 colors: colors,
+                                                                useVertexAlpha: false,
                                                                 updatable: true
                                                             },
                                                             scene));
+        }
+
+        // Construct the lines that constructing the lateral web of lines spanning
+        // all directions.
+        if (this.directions.length > 1) {
+            for (let i = 0; i < this.memory; i += this.latitudeSpacing) {
+                let lineColor = new BABYLON.Color4(0.5, 0.5, 0.5, 1.0)
+                let latitudeLinePoints = [];
+                let colors = [];
+                for (let d = 0; d < this.directions.length; ++d) {
+                    latitudeLinePoints.push(this.points[d][i]);
+                    if (d == this.directions.length - 1) {
+                        latitudeLinePoints.push(this.points[0][i]);
+                    } else {
+                        latitudeLinePoints.push(this.points[d + 1][i]);
+                    }
+                    colors.push(lineColor);
+                    colors.push(lineColor);
+                }
+                this.latitudeLineColors.push(colors);
+                this.latitudeLines.push(latitudeLinePoints);
+                this.latitudePointColors.push(colors);
+                this.latitudeLineMeshes.push(BABYLON.MeshBuilder.CreateLines("lat_lines",
+                                                                             {
+                                                                                 points: latitudeLinePoints,
+                                                                                 colors: colors,
+                                                                                 useVertexAlpha: false,
+                                                                                 updatable: true
+                                                                             },
+                                                                             scene));
+            }
         }
     }
 
@@ -46,11 +84,22 @@ export class Extractor {
                 this.points[d][p].copyFrom(this.points[d][p - 1]).addInPlace(this.directions[d].scale(this.scale));
             }
             this.points[d][0].copyFrom(this.spawner.currentSpawnerPosition.add(this.origin).addInPlace(this.directions[d].scale(this.scale)));
-            this.lines[d] = BABYLON.MeshBuilder.CreateLines("lines",
-                                                            {
-                                                                points: this.points[d],
-                                                                instance: this.lines[d]
-                                                            });
+            this.lineMeshes[d] = BABYLON.MeshBuilder.CreateLines("lines",
+                                                                 {
+                                                                     points: this.points[d],
+                                                                     instance: this.lineMeshes[d]
+                                                                 });
         }
+
+        for (let ll = 0; ll < this.latitudeLines.length; ++ll) {
+            let newColor = this.latitudeLines[ll][0].y / 255;
+            this.latitudePointColors[ll][0].copyFromFloats(newColor, 1.0, 0.5, 1.0);
+            this.latitudeLineMeshes[ll] = BABYLON.MeshBuilder.CreateLines("lat_lines",
+                                                                         {
+                                                                             points: this.latitudeLines[ll],
+                                                                             colors: this.latitudePointColors[ll],
+                                                                             instance: this.latitudeLineMeshes[ll]
+                                                                         });           
+        }        
     }
 }
